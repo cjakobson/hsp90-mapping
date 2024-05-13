e391694412d985a0a7ab9223a3619f363c423918
cjakobson <christopher.jakobson@gmail.com>
2024-05-13 09:49:55 -0700

    initial commit

---
+++ plotting_master_script.m
@@ -0,0 +1,39 @@
+%produce various plots for Hsp90 manuscript
+
+clear
+
+tic
+
+set(0,'DefaultLineLineWidth',1)
+set(0,'DefaultFigureColor','w')
+set(0,'DefaultAxesFontSize',12)
+set(0,'DefaultAxesLineWidth',1)
+
+blue=[43 172 226]./256;
+orange=[248 149 33]./256;
+grey=[128 128 128]./256;
+
+%filebase='/Users/cjakobson/';
+filebase='/Users/christopherjakobson/';
+
+code_directory=[filebase 'Documents/GitHub/hsp90-mapping/'];
+dependency_directory=[filebase '/Dropbox/JaroszLab/hsp90mapping/hsp90-mapping-dependencies/'];
+output_directory=[filebase 'Dropbox/JaroszLab/hsp90mapping/manuscript-plots/'];
+
+addpath([code_directory 'plotting'])
+addpath([code_directory 'plotting/parse'])
+addpath([code_directory 'plotting/calculate'])
+addpath([code_directory 'plotting/plot'])
+
+%Figure 1
+figure('units','normalized','outerposition',[0 0 1 1])
+
+%A
+%n/a
+
+%B
+%rapamycin example segregant data
+%from 
+
+
+
