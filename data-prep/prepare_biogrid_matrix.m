830b7aa78c9d07ec19ef82113a7ddd9206649d2e
cjakobson <christopher.jakobson@gmail.com>
2024-05-28 05:21:40 +0800

    Update prepare_biogrid_matrix.m

--- data-prep/prepare_biogrid_matrix.m
+++ data-prep/prepare_biogrid_matrix.m
@@ -12,10 +12,6 @@ all_genes=all_genes(462:6350);
 
 all_labels=cell(size(all_genes));
 for i=1:length(all_genes)
-
-    if mod(i,100)==0
-        i
-    end
             
     v_temp=ismember(biogrid_data.SystematicNameInteractorA,all_genes{i});
     if sum(v_temp)>0
@@ -37,10 +33,6 @@ interaction_mat=zeros(length(all_genes));
 
 for i=1:length(all_genes)
     
-    if mod(i,100)==0
-        i
-    end
-    
     query_idx=logical(ismember(biogrid_data.SystematicNameInteractorA,all_genes{i})+...
         ismember(biogrid_data.SystematicNameInteractorB,all_genes{i}));
     
@@ -53,10 +45,56 @@ for i=1:length(all_genes)
     
 end
 
-%imagesc(interaction_mat)
-
 %output gene lists and matrix
 save([output_directory 'biogrid_data.mat'],'all_genes','all_labels','interaction_mat')
+
+
+%repeat for physical and genetic interactions separately
+biogrid_data_physical=biogrid_data(ismember(biogrid_data.ExperimentalSystemType,'physical'),:);
+biogrid_data_genetic=biogrid_data(ismember(biogrid_data.ExperimentalSystemType,'genetic'),:);
+
+interaction_mat_physical=zeros(length(all_genes));
+interaction_mat_genetic=zeros(length(all_genes));
+
+for i=1:length(all_genes)
+    
+    query_idx_physical=logical(ismember(biogrid_data_physical.SystematicNameInteractorA,all_genes{i})+...
+        ismember(biogrid_data_physical.SystematicNameInteractorB,all_genes{i}));
+    
+    query_interactors_physical=unique([biogrid_data_physical.SystematicNameInteractorA(query_idx_physical);...
+        biogrid_data_physical.SystematicNameInteractorB(query_idx_physical)]);
+        
+    temp_idx=ismember(all_genes,query_interactors_physical);
+
+    if sum(temp_idx)>0
+    
+        interaction_mat_physical(i,temp_idx)=1;
+        
+    end
+
+    
+    query_idx_genetic=logical(ismember(biogrid_data_genetic.SystematicNameInteractorA,all_genes{i})+...
+        ismember(biogrid_data_genetic.SystematicNameInteractorB,all_genes{i}));
+    
+    query_interactors_genetic=unique([biogrid_data_genetic.SystematicNameInteractorA(query_idx_genetic);...
+        biogrid_data_genetic.SystematicNameInteractorB(query_idx_genetic)]);
+        
+    temp_idx=ismember(all_genes,query_interactors_genetic);
+
+    if sum(temp_idx)>0
+    
+        interaction_mat_genetic(i,temp_idx)=1;
+        
+    end
+end
+
+
+save([output_directory 'biogrid_data_physical.mat'],'all_genes','all_labels',...
+    'interaction_mat_physical')
+save([output_directory 'biogrid_data_genetic.mat'],'all_genes','all_labels',...
+    'interaction_mat_genetic')
+
+
 
 toc
 
